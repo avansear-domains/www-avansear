4ff4fd0e211166ee91327f84e7c012953db26957
avan <100318118+avansear@users.noreply.github.com>
2025-10-02 16:31:35 -0400

    teeny tiny fix

--- app/musix/archive.ts
+++ app/musix/archive.ts
@@ -8,55 +8,55 @@ export interface ArchivedSong {
 export function getArchivedSongs(): ArchivedSong[] {
   return [
     {
-      week: "week 1",
+      week: "week 01",
       songName: "kilby girl",
       artist: "the backseat lovers",
       youtubeId: "oTwVce9eWb4"
     },
     {
-      week: "week 2",
+      week: "week 02",
       songName: "dead girl in the pool.",
       artist: "girl in red",
       youtubeId: "Ra9KtiCMynE"
     },
     {
-      week: "week 3",
+      week: "week 03",
       songName: "better",
       artist: "regina spektor",
       youtubeId: "eAR2iHYNIe4"
     },
     {
-      week: "week 4",
+      week: "week 04",
       songName: "why this kolaveri di?",
       artist: "dhanush",
       youtubeId: "geIAO3PrE2g"
     },
     {
-      week: "week 5",
+      week: "week 05",
       songName: "motels",
       artist: "royel otis",
       youtubeId: "_J3Lks5GVQs"
     },
     {
-      week: "week 6",
+      week: "week 06",
       songName: "bloom",
       artist: "the paper kites",
       youtubeId: "Cyx2CWBVfDE"
     },
     {
-      week: "week 7",
+      week: "week 07",
       songName: "brazil",
       artist: "declan mckenna",
       youtubeId: "sSUecTSYulM"
     },
     {
-      week: "week 8",
+      week: "week 08",
       songName: "memoria (burnt edit)",
       artist: "philip mchale",
       youtubeId: "RREc6aKHk-M"
     },
     {
-      week: "week 9",
+      week: "week 09",
       songName: "anything",
       artist: "adrianne lenker",
       youtubeId: "pftT6MhrtLE"
