21e37e5a58be58fe1e952b884e9f72a860fb4a7f
avan <100318118+avansear@users.noreply.github.com>
2025-11-22 15:53:06 -0500

    lost page LMAo

---
+++ app/lost/page.tsx
@@ -0,0 +1,43 @@
+'use client'
+
+import { motion } from 'motion/react'
+import { useState } from 'react'
+import { AnimatedHeading } from 'app/components/animated-heading'
+
+export default function Page() {
+  const [isHovered, setIsHovered] = useState(false)
+
+  return (
+    <section>
+      <div className="p-4 -m-4">
+        <AnimatedHeading 
+          className="sm:mb-8 text-2xl font-semibold tracking-tighter"
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+        >
+          <span>i'm avan</span>
+          <motion.span
+            animate={{ opacity: isHovered ? 1 : 0 }}
+            transition={{ 
+              duration: 0.3,
+              ease: "easeOut"
+            }}
+          >
+            sear (vishruth siddi)
+          </motion.span>
+        </AnimatedHeading>
+      </div>
+      
+      <p className="sm:mb-4">
+        {`if you're seeing this page because you tapped on an nfc sticker on a random device you found, you're in the right place!`}
+      </p>
+      <p className="sm:mb-4">
+        {`i put nfc stickers on all my devices in case they ever get lost. please use the below contact details to get in touch with me. thank you so much for your help!`}
+      </p>
+      <p className="sm:mb-4">
+        {`in case you choose not to return this, then boooo :(`}
+      </p>
+    </section>
+  )
+}
+
