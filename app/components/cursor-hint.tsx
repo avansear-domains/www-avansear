c3ae18aa52f7096fa557f3ccc09229a778754cab
avan <100318118+avansear@users.noreply.github.com>
2025-12-16 14:44:19 -0500

    updated overflow

--- app/components/cursor-hint.tsx
+++ app/components/cursor-hint.tsx
@@ -6,7 +6,7 @@ export function CursorHint() {
   const { isCursorEnabled } = useCursor()
 
   return (
-    <div className="fixed bottom-4 right-4 z-40 font-mono text-xs text-[var(--color-dark)]">
+    <div className="hidden md:block fixed bottom-4 right-4 z-40 font-mono text-xs text-[var(--color-dark)]">
       <div className="bg-[var(--color-light-80)] px-2 py-1 rounded-lg">
         <kbd>ctrl + shift + space</kbd>
         <span className="ml-1.5">
